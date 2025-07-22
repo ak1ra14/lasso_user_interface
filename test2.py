2976a63829ca34276026266bf3070ccee3e59429
ak1ra14 <asat0013@student.monash.edu>
2025-07-22 22:26:37 +0800

    added another testing

--- test2.py
+++ test2.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.clock import Clock
+from utils.config_loader import load_config
+from utils.layout import HeaderBar
 
 class MonitorScreen(Screen):
     def __init__(self, **kwargs):
@@ -12,17 +14,24 @@ class MonitorScreen(Screen):
 
     def go_to_menu(self, instance):
         print("Button clicked, going to Menu")
-        Clock.schedule_once(lambda dt: setattr(self.manager, 'current', 'menu'), 0)
+        Clock.schedule_once(lambda dt: setattr(self.manager, 'current', 'test'), 0)
 
-class MenuScreen(Screen):
-    def on_enter(self):
-        print("MenuScreen entered")
+class TestScreen(Screen):
+    def __init__(self, **kwargs):
+        super(TestScreen, self).__init__(**kwargs)
+        self.config = load_config('config/V3.json')  # Read once
+        self.location = self.config.get("location", "Room 101")
+        volume = self.config.get("volume", 50)
+
+        # Add header bar
+        self.header = HeaderBar(title="Test Screen")
+        self.add_widget(self.header)
 
 class TestApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MonitorScreen(name='monitor'))
-        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(TestScreen(name='test'))
         return sm
 
 TestApp().run()
