76fc6a6a92bdc80a907b2d5448f8f9281909fdcc
ak1ra14 <asat0013@student.monash.edu>
2025-07-26 18:14:10 +0800

    testing wifi list

--- screens/wifi.py
+++ screens/wifi.py
@@ -5,13 +5,21 @@ from utils.layout import HeaderBar
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
 from kivy.clock import Clock
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.graphics import Color, Rectangle
+from kivy.uix.button import Button
+from utils.config_loader import load_config, save_config
 
 class WifiLoadingScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.config = load_config("config/V3.json")
+        self.selected_wifi = self.config.get('wifi_ssid', {})
         self.header = HeaderBar(title=" ")
         self.add_widget(self.header)
         self.add_widget(Label(text="Scanning WiFi...", size_hint=(1, 0.1), pos_hint={'center_x': 0.5, 'center_y': 0.5}))
+        self.wifi_list = []
 
     def on_pre_enter(self):
         # Start scanning in a background thread when the screen is shown
@@ -25,11 +33,39 @@ class WifiLoadingScreen(Screen):
     def show_results(self, wifi_list):
         self.clear_widgets()
         self.add_widget(self.header)
-        if wifi_list:
-            for ssid in wifi_list:
-                self.add_widget(Label(text=ssid, size_hint=(1, None), height=40))
-        else:
-            self.add_widget(Label(text="No WiFi networks found.", size_hint=(1, None), height=40))
+        list_box = GridLayout(cols=1, size=(465, 800), size_hint=(None,None), pos_hint=(None, None),)
+        for wifi in wifi_list:
+            button = SelectableButton(text=wifi, font_size=40,
+                                                font_family='fonts/Roboto-Bold.ttf', size_hint_y=None, 
+                                                height=55, selection=self)
+            list_box.add_widget(button)
+            self.wifi_list.append(button)
+            
+        scroll = ScrollView(size_hint=(None, None),
+                            scroll_type=['bars', 'content'],
+                            bar_width=35, 
+                            bar_color=(0.2, 0.6, 0.8, 1),  # Active bar color (blue)
+                            bar_inactive_color=(0.7, 0.7, 0.7, 1),  # Inactive bar color (gray)
+                            size=(500,300),
+                            do_scroll_x=False)
+        with scroll.canvas.before:
+            Color(1, 1, 1, 1)  # White
+            rect = Rectangle(pos=scroll.pos, size=scroll.size)
+        scroll.bind(pos=lambda instance, value: setattr(rect, 'pos', value))
+        scroll.bind(size=lambda instance, value: setattr(rect, 'size', value))
+                
+        scroll.add_widget(list_box)
+        self.add_widget(scroll)
+
+    def select_wifi(self, btn):
+        for b in self.timezone_list:
+            if b == btn:
+                continue
+            b.selected = False
+            b.update_color()
+        btn.selected = True
+        btn.update_color()
+        self.selected_wifi = btn.text
 
 
 def get_available_wifi():
@@ -64,6 +100,48 @@ def get_available_wifi():
         except Exception as e:
             print("Error getting Wi-Fi:", e)
     return wifi_list
+
+
+class SelectableButton(Button):
+    """
+    A button that can be selected or deselected.
+    """
+    def __init__(self, text,selection, **kwargs):
+        super().__init__(**kwargs)
+        self.text = text
+        self.selection = selection  # Reference to the selection manage
+        self.selected = (selection.selected_wifi == text)  # Check if this button's timezone is the selected one
+        self.background_normal = ''
+        self.background_down = ''
+        self.background_color = (1, 1, 1, 1)
+        with self.canvas.before:
+            Color(1, 1, 1, 1)
+            self.rect = Rectangle(pos=self.pos, size=self.size)
+        self.bind(pos=self._update_rect, size=self._update_rect)
+        self.update_color()
+        self.bind(on_relase=self.on_press)
+    
+    def _update_rect(self, *args):
+        """
+        Update the rectangle size and position when the button is resized or moved.
+        """
+        self.rect.pos = self.pos
+        self.rect.size = self.size
+
+    def update_color(self):
+        print(self.text, "selected:", self.selected)
+        if self.selected:
+            self.color = (1, 1, 1, 1)
+            self.background_color = (0.2, 0.6, 0.8, 1)
+        else:
+            self.color = (0, 0, 0, 1)
+            self.background_color = (1, 1, 1, 1)
+
+    def on_press(self):
+        """
+        Override the on_press method to handle button press.
+        """
+        super().on_press()
 
 # Example usage:
 if __name__ == "__main__":
