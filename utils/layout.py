18043104ea39227f42bc06d85ad4860cda793447
ak1ra14 <asat0013@student.monash.edu>
2025-08-09 23:41:06 +0900

    added conditional positioning

--- utils/layout.py
+++ utils/layout.py
@@ -21,8 +21,8 @@ class HeaderBar(BoxLayout):
             font_name='fonts/MPLUS1p-Bold.ttf',
             halign='left',
             valign='middle',
-            width = 800
-        ))
+            size_hint_x = 0.7,
+     ))
         self.title_label.bind(size=lambda inst, val: setattr(inst, 'text_size', val))
         self.add_widget(self.title_label)
         self.add_widget(Widget(size_hint_x=1))  # Spacer
