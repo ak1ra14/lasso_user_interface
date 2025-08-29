521da8439732c104f81515fd98438368858f443d
ak1ra14 <asat0013@student.monash.edu>
2025-08-29 23:43:01 +0900

    adding print

--- main.py
+++ main.py
@@ -207,7 +207,8 @@ class MyApp(App):
                 self.config['wifi_ssid'] = ssid
                 save_config('config/settings.json', 'v3_json', data=self.config)
                 self.ip_address = get_ip_address()
-                print(self.ip_address)
+                monitor_screen = self.sm.get_screen('monitor')
+                monitor_screen.ip_label.text = f"{update_text_language('ip_address')}: {self.ip_address}"
             else:
                 self.config['wifi_ssid'] = 'Not connected'
                 save_config('config/settings.json', 'v3_json', data=self.config)
