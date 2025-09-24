c31d36b92b97a1969c18497867224897bebd1100
ak1ra14 <asat0013@student.monash.edu>
2025-09-24 20:37:27 +0900

    added separator line for wifi options

--- screens/wifi.py
+++ screens/wifi.py
@@ -566,7 +566,7 @@ class SelectableButton(Button):
             self.rect = Rectangle(pos=self.pos, size=self.size)
         with self.canvas.after:
             Color(0.8, 0.8, 0.8, 1)  # Light gray
-            self.separator = Rectangle(pos=(self.x, self.y), size=(self.width, 3))
+            self.separator = Rectangle(pos=(self.x, self.y), size=(self.width, 1))
         self.bind(pos=self._update_rect, size=self._update_rect)
         self.bind(pos=self._update_separator, size=self._update_separator)
         self.bind(pos=self._update_rect, size=self._update_rect)
@@ -584,9 +584,9 @@ class SelectableButton(Button):
         self.rect.size = self.size
 
     def _update_separator(self, *args):
-        # Position separator at the bottom of the button
+        # Position separator at the bottom of the button, thickness 3px
         self.separator.pos = (self.x, self.y)
-        self.separator.size = (self.width, 1)
+        self.separator.size = (self.width, 3)
 
     def update_color(self):
         #print(self.text, "selected:", self.selected)
