ebcf7f0f00c91fe767406279fd20fbb05e6d1f84
ak1ra14 <asat0013@student.monash.edu>
2025-10-01 14:34:02 +0900

    updated image name

--- as_utils/as_connection_manager.py
+++ as_utils/as_connection_manager.py
@@ -122,7 +122,7 @@ class ConnectionManager:
 
             menu_screen = self.app.sm.get_screen('menu2')
             menu_screen.content_buttons['wi-fi'].status.text = config['wifi_ssid'] if config['wifi_ssid'] != 'Not connected' else update_text_language('not_connected')
-            menu_screen.content_buttons['wi-fi'].image.source = "as_images/wifi.png" if config['wifi_ssid'] != 'Not connected' else "images/wifi_not_connected.png"
+            menu_screen.content_buttons['wi-fi'].image.source = "as_images/wifi.png" if config['wifi_ssid'] != 'Not connected' else "as_images/wifi_not_connected.png"
 
             # Save updated config
             save_config('as_config/settings.json', 'v3_json', data=config)
